8ab64f1caded67718c87d1467523a14f9362e129
anaelle <anaelle.fourre@gmail.com>
2025-04-14 17:34:06 +0200

    feat: add straight track

--- src/draw_scene.cpp
+++ src/draw_scene.cpp
@@ -10,6 +10,10 @@ int grid_size {10};
 GLBI_Engine myEngine;
 GLBI_Set_Of_Points frame(3);
 GLBI_Convex_2D_Shape grid{3};
+GLBI_Convex_2D_Shape disc{3};
+GLBI_Convex_2D_Shape curved_rail{3};
+IndexedMesh* cube;
+IndexedMesh* cylinder;
 
 
 void initScene() {
@@ -38,6 +42,28 @@ void initScene() {
 	};
 	frame.initSet(framePoints, frameColors);
 	frame.changeNature(GL_LINES);
+
+	// Disc
+	std::vector<float> discPoints;
+	float angle;
+	for (auto i = 0; i < 360; i++) {
+		angle = M_PI * i / 180;
+		discPoints.push_back(cos(angle));
+		discPoints.push_back(sin(angle));
+		discPoints.push_back(0.);
+	}
+	disc.initShape(discPoints);
+	disc.changeNature(GL_TRIANGLE_FAN);
+
+
+	// Cube
+	cube = basicCube();
+	cube->createVAO();
+
+	// Cylinder
+	cylinder = basicCylinder(1., 1.);
+	cylinder->createVAO();
+
 }
 
 void drawGrid() {
@@ -53,15 +79,71 @@ void drawGrid() {
 	}
 }
 
+void drawRail() {
+	myEngine.setFlatColor(140. / 255, 140. / 255., 140. / 255.);
+	myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addHomothety(Vector3D(sr, 10., sr));
+		myEngine.mvMatrixStack.addTranslation(Vector3D(0.5, 0.5, 0.5));
+		myEngine.updateMvMatrix();
+		cube->draw();
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawBalast() {
+	myEngine.setFlatColor(153. / 255, 102. / 255., 51. / 255.);
+	myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addHomothety(Vector3D(6., rr, rr));
+		myEngine.mvMatrixStack.addTranslation(Vector3D(0., 1., 1.));
+		myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addRotation(M_PI / 2, Vector3D(0., 1., 0.));
+			myEngine.updateMvMatrix();
+			disc.drawShape(); 
+			myEngine.mvMatrixStack.pushMatrix();
+				myEngine.mvMatrixStack.addTranslation(Vector3D(0., 0., 1.));
+				myEngine.updateMvMatrix();
+				disc.drawShape();
+			myEngine.mvMatrixStack.popMatrix();
+		myEngine.mvMatrixStack.popMatrix(); 
+		myEngine.mvMatrixStack.pushMatrix();
+			myEngine.mvMatrixStack.addRotation(3 * M_PI / 2, Vector3D(0., 0., 1.));
+			myEngine.updateMvMatrix();
+			cylinder->draw();
+		myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.popMatrix();
+}
+
+void drawStraightTrack() {
+	myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addTranslation(Vector3D(POS_X_BALAST1, 0., 0.));
+		for (auto i = 0; i < 5; i++) {
+			myEngine.mvMatrixStack.pushMatrix();
+				myEngine.mvMatrixStack.addTranslation(Vector3D(0., sx + i * 2 * sx + i * 2 * rr, 0.));
+				drawBalast();
+			myEngine.mvMatrixStack.popMatrix();
+		}
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addTranslation(Vector3D(POS_X_RAIL1, 0., 2 * rr));
+		drawRail();
+	myEngine.mvMatrixStack.popMatrix();
+	myEngine.mvMatrixStack.pushMatrix();
+		myEngine.mvMatrixStack.addTranslation(Vector3D(POS_X_RAIL2, 0., 2 * rr));
+		drawRail();
+	myEngine.mvMatrixStack.popMatrix();
+}
+
 void drawFrame() {
 	frame.drawSet();
 }
+
 
 void drawScene() {
 	glPointSize(10.0);
 	
 	drawFrame();
 	drawGrid();
+
+	drawStraightTrack();
 }
 
 
--- src/draw_scene.hpp
+++ src/draw_scene.hpp
@@ -10,6 +10,13 @@ using namespace glbasimac;
 /* Camera parameters and functions */
 static const float Z_NEAR {0.1f};
 static const float Z_FAR {500.f};
+static const float POS_X_RAIL1 {3.};
+static const float POS_X_RAIL2 {7.};
+static const float POS_X_BALAST1 {2.};
+static const float POS_X_BALAST2 {8.};
+static const float sr {0.5};
+static const float rr {0.3};
+static const float sx {(10. - 10 * rr) / 10.};
 extern float angle_theta;      // Angle between x axis and viewpoint
 extern float angle_phy  ;      // Angle between z axis and viewpoint
 extern float dist_zoom  ;      // Distance between origin and viewpoint
