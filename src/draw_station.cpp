24590302ed23c43e3f8e1766c3412f3667676fdb
anaelle <anaelle.fourre@gmail.com>
2025-04-23 17:50:10 +0200

    feat(texture) : add brick texture to station

--- src/draw_station.cpp
+++ src/draw_station.cpp
@@ -1,8 +1,11 @@
 #include "draw_station.hpp"
 #include "draw_scene.hpp"
+#include "glbasimac/glbi_texture.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include <tools/stb_image.h>
 
 GLBI_Convex_2D_Shape side_roof{3};
-
+GLBI_Texture brick_texture;
 
 void initStation()
 {
@@ -14,7 +17,20 @@ void initStation()
     };
     side_roof.initShape(sr_points);
     side_roof.changeNature(GL_TRIANGLE_FAN);
+    
+    
+    int width, height, channels;
+    auto image = stbi_load("../assets/textures/brick.jpg", &width, &height, &channels, 0);
+    if (image == NULL) {
+        std::cerr << "Unable to load ../assets/textures/brick.jpg" << std::endl;
+    }
+    
+    brick_texture.createTexture();
 
+    brick_texture.attachTexture();
+    brick_texture.loadImage(width, height, channels, image);
+    brick_texture.setParameters(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    brick_texture.detachTexture();
 }
 
 void drawCenterRoof() {
@@ -55,6 +71,19 @@ void drawSideRoof() {
     myEngine.mvMatrixStack.popMatrix();
 }
 
+void drawBrickBuilding() {
+    myEngine.mvMatrixStack.pushMatrix();
+        myEngine.mvMatrixStack.addRotation(M_PI_2, Vector3D(1., 0., 0.));
+        myEngine.mvMatrixStack.addRotation(M_PI_2, Vector3D(0., 1., 0.));
+        myEngine.updateMvMatrix();
+        myEngine.activateTexturing(true);            
+        brick_texture.attachTexture();
+        cube->draw();
+        brick_texture.detachTexture();
+        myEngine.activateTexturing(false);
+    myEngine.mvMatrixStack.popMatrix();
+}
+
 void drawSideBuilding() {
     myEngine.mvMatrixStack.pushMatrix();
         myEngine.setFlatColor(153. / 255, 51. / 255., 0.);
@@ -62,7 +91,7 @@ void drawSideBuilding() {
             myEngine.mvMatrixStack.addHomothety(Vector3D(4., 2., 3.));
             myEngine.mvMatrixStack.addTranslation(Vector3D(0.5, 0.5, 0.5));
             myEngine.updateMvMatrix();
-            cube->draw();
+            drawBrickBuilding();
         myEngine.mvMatrixStack.popMatrix();
         myEngine.setFlatColor(51. / 255, 26. / 255., 0.);
         myEngine.mvMatrixStack.addTranslation(Vector3D{0., 0., 3.});
@@ -77,7 +106,7 @@ void drawCenterBuilding() {
         myEngine.mvMatrixStack.addHomothety(Vector3D(6., 4., 4.));
         myEngine.mvMatrixStack.addTranslation(Vector3D(0.5, 0.5, 0.5));
         myEngine.updateMvMatrix();
-        cube->draw();
+        drawBrickBuilding();
     myEngine.mvMatrixStack.popMatrix();
     myEngine.mvMatrixStack.pushMatrix();
         myEngine.setFlatColor(51. / 255, 26. / 255., 0.);
